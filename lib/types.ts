e4255bb3a868764c8fb6e15237c838515ff9c7a1
myferr <contactme.myfer@protonmail.com>
2026-01-04 18:19:08 -0600

    feat: add TypeScript type definitions

---
+++ lib/types.ts
@@ -0,0 +1,21 @@
+import type { t } from "./t";
+
+type SchemaType = ReturnType<
+	| typeof t.string
+	| typeof t.number
+	| typeof t.boolean
+	| typeof t.object
+	| typeof t.array
+	| typeof t.any
+>;
+
+type DictionaryOptions = {
+	env: Record<string, unknown>;
+	schema?: Record<string, SchemaType>;
+};
+
+type DictionaryEnvDescriptor<T extends string, V> = { [K in T]: V } & {
+	type: SchemaType;
+};
+
+export type { SchemaType, DictionaryOptions, DictionaryEnvDescriptor }
